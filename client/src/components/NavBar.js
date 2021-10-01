cefd2bb99ad01c4606c32d6459ad317cb3563db8
Theadejong <theadejong85@gmail.com>
2021-10-01 18:28:28 +0200

    pull

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-import { Link } from "react-router-dom";
-import { useContext } from "react";                       // <== IMPORT 
-import { AuthContext } from "../context/auth.context";    // <== IMPORT
-
-export default function NavBar() {
-
-    const { isLoggedIn, user, logOutUser } = useContext(AuthContext); 
-
-    return (
-        <div className="NavBar">
-            <ul>
-                <li>
-                    <Link to='/'>Home</Link>
-                    <Link to='/wines'>Discover wines</Link>
-                    <Link to='/WineMap'>Wine map</Link>
-                    <Link to='/search'>Search</Link>
-                    <Link to='/AboutUs'>About Us</Link>
-                    {isLoggedIn
-                    ? (<>
-                    <Link to="/mywinecellar">My Winecellar</Link>
-                    <button onClick={logOutUser}>Logout</button>
-                    <span>{user.name}</span>
-                    </>)
-                    :
-                    (<>
-                    <Link to='/UserLogin'>Log in</Link>
-                    <Link to='/SignUp'>Sign Up</Link>
-                    </>)
-                    }
-                </li>
-            </ul>
-=======
 import React, { useState } from 'react';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
@@ -51,7 +18,6 @@ function Navbar() {
           <Link to='#' className='menu-bars'>
             <FaIcons.FaBars onClick={showSidebar} />
           </Link>
->>>>>>> 8d0292db0f595f3a6c6350283691bb04f28074c3
         </div>
         <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
           <ul className='nav-menu-items' onClick={showSidebar}>
