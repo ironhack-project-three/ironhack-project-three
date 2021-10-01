75533b24e74d49785da6f413bacbfa5c4d5ed68f
Coops023 <bjorkc27@gmail.com>
2021-10-01 12:35:51 +0200

    autherisation wip

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const mongoose = require('mongoose');
-
+const jwt = require('jsonwebtoken')
 const User = require("../models/User.model")
 const Service = require("../services/service")
 const bcrypt = require('bcrypt')
-const saltRound = 10;
+const saltRounds = 10;
 
 /* GET list of Users. */
 router.get('/all-users', (req, res)=> {
@@ -17,14 +17,52 @@ router.get('/all-users', (req, res)=> {
 /* Fill all the Users CRUD Routes */
 //User Create route
 router.post('/create-user', (req, res)=> {
-  const {username, email, password} = req.body;
-  const salt = bcrypt.genSaltSync(saltRound);
-  const hashPassword = bcrypt.hashSync(password, salt);
-  console.log("error")
-  User.create({username, email})
-  .then((newUser)=>{res.json({newUser})})
-  .catch(error => console.log(error))
-//password: hashPassword
+  const {username, email, password } = req.body;
+  // Check if email or password or name are provided as empty string 
+  if (username === '' || email === '' || password === '') {
+    res.status(400).json({ message: "Provide email, password and name" });
+    return;
+  }
+  // Use regex to validate the email format
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+  if (!emailRegex.test(email)) {
+    res.status(400).json({ message: 'Provide a valid email address.' });
+    return;
+  }
+  // Use regex to validate the password format
+  const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!passwordRegex.test(password)) {
+    res.status(400).json({ message: 'Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.' });
+    return;
+  }
+  // Check the users collection if a user with the same email already exists
+  User.findOne({ email })
+    .then((foundUser) => {
+      // If the user with the same email already exists, send an error response
+      if (foundUser) {
+        res.status(400).json({ message: "User already exists." });
+        return;
+      }
+      // If email is unique, proceed to hash the password
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hashedPassword = bcrypt.hashSync(password, salt);
+      // Create the new user in the database
+      // We return a pending promise, which allows us to chain another `then` 
+      return User.create({username, email, password: hashedPassword });
+    })
+    .then((createdUser) => {
+      // Deconstruct the newly created user object to omit the password
+      // We should never expose passwords publicly
+      const {username, email, _id } = createdUser;
+      // Create a new object that doesn't expose the password
+      const user = {username, email, _id };
+      // Send a json response containing the user object
+      res.status(201).json({ user: user });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "Internal Server Error" })
+    });
 });
 
 //Get user by specific id
@@ -76,6 +114,56 @@ router.delete('/user/:userId', (req, res, next) => {
     .then(() => res.json({ message: `User with ${userId} is removed successfully.` }))
     .catch(error => res.json(error));
 })
+
+// POST  /auth/login - Verifies email and password and returns a JWT
+router.post('/login', (req, res, next) => {
+  
+  const { email, password} = req.body;
+ console.log("line 122", email, password)
+  // Check if email or password are provided as empty string 
+  if (email === '' || password === '') {
+    res.status(400).json({ message: "Provide email and password." });
+    return;
+  }
+ 
+  // Check the users collection if a user with the same email exists
+  User.findOne({ email })
+    .then((foundUser) => {
+      console.log('line 132', foundUser)
+      if (!foundUser) {
+        // If the user is not found, send an error response
+        res.status(401).json({ message: "User not found." })
+        return;
+      }
+ 
+      // Compare the provided password with the one saved in the database
+      const passwordCorrect = bcrypt.compareSync(password, foundUser.password);
+ 
+      if (passwordCorrect) {
+        // Deconstruct the user object to omit the password
+        const { _id, username, email} = foundUser;
+        
+        // Create an object that will be set as the token payload
+        const payload = { _id, username, email};
+ 
+        // Create and sign the token
+        const authToken = jwt.sign( 
+          payload,
+          process.env.TOKEN_SECRET,
+          { algorithm: 'HS256', expiresIn: "6h" }
+        );
+ 
+        // Send the token as the response
+        res.status(200).json({ authToken: authToken });
+      }
+      else {
+        res.status(401).json({ message: "Unable to authenticate the user" });
+      }
+ 
+    })
+    .catch(err => res.status(500).json({ message: "Internal Server Error" }));
+});
+
 
 
 module.exports = router;
