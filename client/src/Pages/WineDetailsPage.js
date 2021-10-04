d638d086b1f233845aba11fcc795b36362df98eb
Jesse Vermeulen <jessevermeulen123@gmail.com>
2021-10-04 14:59:46 +0200

    WIP: wine details page

--- client/src/Pages/WineDetailsPage.js
+++ client/src/Pages/WineDetailsPage.js
@@ -1,33 +1,44 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import { Box } from "react-bulma-components";
+import { Block, Box } from "react-bulma-components";
 import { Wines } from "../api/wines";
+import { FaSpinner } from "react-icons/fa";
 
 export default function WineDetailsPage(props) {
   const [wine, setWine] = useState({});
-  async function fetchWine() {
-    const response = await new Wines().getOne(props.match.params.id);
+  const [loading, setLoading] = useState(true);
+
+  async function fetchWine(id) {
+    const response = await new Wines().getOne(id);
     console.log("this is the response", response);
     console.log("this is the response data", response.data);
     setWine(response.data);
+    setLoading(false);
   }
 
   useEffect(() => {
-    fetchWine();
+    fetchWine(props.match.params.id);
   }, [props.match.params.id]);
 
   return (
     <div>
-      <h1 className="title is-size-1">WineDetailsPage</h1>
-        <Box className="title is-size-3">{wine.title}</Box>
-        <Box className="title is-size-3">{wine.description}</Box>
-        <Box className="title is-size-3">{wine.pricing}</Box>
-        <Box className="title is-size-3">{wine.variety}</Box>
-        <Box className="title is-size-3">{wine.region_1}</Box>
-        <Box className="title is-size-3">{wine.province}</Box>
-        <Box className="title is-size-3">{wine.country}</Box>
-        <Box className="title is-size-3">{wine.winery}</Box>
-        <img src="https://www.placecage.com/500/500" />
+      <Box className="title is-size-1">{wine.title}</Box>
+      {
+        loading ? 
+          <div style={{ fontSize: "128px" }}>
+            <FaSpinner className="fa-spin" />
+          </div>
+        :
+          <Box>
+            <Block className=""><b>Description:</b> {wine.description}</Block>
+            <Block className=""><b>Price Range:</b> $ {wine.price} (RRP)</Block>
+            <Block className=""><b>Variety:</b> {wine.variety}</Block>
+            <Block className=""><b>Region:</b> {wine.region_1}</Block>
+            <Block className=""><b>Province:</b> {wine.province}</Block>
+            <Block className=""><b>Country:</b> {wine.country}</Block>
+            <Block className=""><b>Winery:</b> {wine.winery}</Block>
+          </Box>
+      }
     </div>
   );
 }
