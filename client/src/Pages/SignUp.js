47b1a6b97b31d60886925c7165d7c81c2c3320c5
Theadejong <theadejong85@gmail.com>
2021-10-05 21:37:35 +0200

    commit

--- client/src/Pages/SignUp.js
+++ client/src/Pages/SignUp.js
@@ -1,33 +1,21 @@
-import React from "react";
+import React from 'react';
 import { Link } from "react-router-dom";
-import { useState } from "react";
-import { motion } from "framer-motion";
-import { Users } from "../api/users";
-
-export default function SignUp(props) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [username, setUserName] = useState("");
-  const [errorMessage, setErrorMessage] = useState(undefined);
-
-
+import {useState} from 'react';
+import axios from "axios";
+import { motion } from "framer-motion"
 // const API_URL = "http://localhost:3000";
 // const frontUrl = "http://localhost:5000"
 let baseURL = process.env.REACT_APP_API_URL || "http://localhost:3000"
  
-
-
 export default function SignUp(props) {
         const [email, setEmail] = useState("");
         const [password, setPassword] = useState("");
         const [username, setUserName] = useState("");
         const [errorMessage, setErrorMessage] = useState(undefined);
-      
         const handleEmail = (e) => setEmail(e.target.value);
         const handlePassword = (e) => setPassword(e.target.value);
         const handleUserName = (e) => setUserName(e.target.value);
         const handleSignupSubmit = (e) => { e.preventDefault();
-          
           // Create an object representing the request body
           const requestBody = {username, email, password};
           const storedToken = localStorage.getItem('authToken');
@@ -50,163 +38,76 @@ export default function SignUp(props) {
                 <div className="container">
                     <div className="columns is-centered">
                         <div className="column is-5-tablet is-4-desktop is-3-widescreen">
-                            <form 
-                            action="/users/create-user" 
-                            method="POST" 
-                            className="box" 
+                            <form
+                            action="/users/create-user"
+                            method="POST"
+                            className="box"
                             onSubmit={handleSignupSubmit}>
                                 <h3 className="is-3">Sign up</h3>
                                 <div className="field" style={{fontSize: "12px"}}>
-                             
                                 </div>
                                 <div className="field">
-                                    <label 
-                                    for="username" 
+                                    <label
+                                    for="username"
                                     className="label">Username
                                     </label>
                                     <div className="control has-icons-left">
-                                        <input  
-                                        type="username" 
-                                        placeholder="e.g. VineVibe" 
-                                        className="input" 
-                                        value={username} 
-                                        onChange={handleUserName} 
+                                        <input
+                                        type="username"
+                                        placeholder="e.g. VineVibe"
+                                        className="input"
+                                        value={username}
+                                        onChange={handleUserName}
                                         required></input>
                                     </div>
                                 </div>
                                 <div className="field">
-                                    <label 
-                                    for="email" 
+                                    <label
+                                    for="email"
                                     className="label">Email
                                     </label>
                                     <div className="control has-icons-left">
-                                        <input 
-                                        for="email" 
-                                        type="email" 
-                                        placeholder="e.g. your_email@VineVibe.com" 
-                                        className="input" 
-                                        value={email} 
-                                        onChange={handleEmail} 
+                                        <input
+                                        for="email"
+                                        type="email"
+                                        placeholder="e.g. your_email@VineVibe.com"
+                                        className="input"
+                                        value={email}
+                                        onChange={handleEmail}
                                         required></input>
                                     </div>
                                 </div>
                                 <div className="field">
-                                    <label 
-                                    for="password" 
-                                    type="password" 
+                                    <label
+                                    for="password"
+                                    type="password"
                                     className="label">Password
                                     </label>
                                     <div className="control has-icons-left">
-                                        <input 
-                                        type="password" 
-                                        placeholder="********" 
-                                        className="input" 
-                                        value={password} 
-                                        onChange={handlePassword} 
+                                        <input
+                                        type="password"
+                                        placeholder="********"
+                                        className="input"
+                                        value={password}
+                                        onChange={handlePassword}
                                         required></input>
                                     </div>
                                     <div className='field' style={{fontSize: "12px"}}>
                                         <Link to={"/Userlogin"}>Already have an account? Login here</Link>
                                     </div>
                                     <div className="field">
-
-
-    // Make an axios request to the API
-    // If POST request is successful redirect to login page
-    // If the request resolves with an error, set the error message in the state
-    try {
-      await Users.createOne(requestBody, storedToken);
-      props.history.push(`/login`);
-    } catch (error) {
-      console.log("line 31", error.response);
-      const errorDescription = error.response.data.message;
-      setErrorMessage(errorDescription);
-    }
-  };
-
-  return (
-    <section className="hero has-background-white-ter is-fullheight">
-      <div className="hero-body">
-        <div className="container">
-          <div className="columns is-centered">
-            <div className="column is-5-tablet is-4-desktop is-3-widescreen">
-              <form
-                action="/users/create-user"
-                method="POST"
-                className="box"
-                onSubmit={handleSignupSubmit}
-              >
-                <h3 className="is-3">Sign up</h3>
-                <div className="field" style={{ fontSize: "12px" }}>
-                  {errorMessage}
+                                        <motion.button
+                                        whileHover = {{ scale: 1.1}}
+                                        whileTap = {{ scale: 0.9}}
+                                        type="submit" className="button is-warning is-rounded">Sign up</motion.button>
+                                    </div>
+                                </div>
+                            </form>
+                            { errorMessage && <p className="error-message">{errorMessage}</p> }
+                        </div>
+                    </div>
                 </div>
-                <div className="field">
-                  <label for="username" className="label">
-                    Username
-                  </label>
-                  <div className="control has-icons-left">
-                    <input
-                      type="username"
-                      placeholder="e.g. VineVibe"
-                      className="input"
-                      value={username}
-                      onChange={handleUserName}
-                      required
-                    ></input>
-                  </div>
-                </div>
-                <div className="field">
-                  <label for="email" className="label">
-                    Email
-                  </label>
-                  <div className="control has-icons-left">
-                    <input
-                      for="email"
-                      type="email"
-                      placeholder="e.g. your_email@VineVibe.com"
-                      className="input"
-                      value={email}
-                      onChange={handleEmail}
-                      required
-                    ></input>
-                  </div>
-                </div>
-                <div className="field">
-                  <label for="password" type="password" className="label">
-                    Password
-                  </label>
-                  <div className="control has-icons-left">
-                    <input
-                      type="password"
-                      placeholder="********"
-                      className="input"
-                      value={password}
-                      onChange={handlePassword}
-                      required
-                    ></input>
-                  </div>
-                  <div className="field" style={{ fontSize: "12px" }}>
-                    <Link to={"/UserLogin"}>
-                      Already have an account? Login here
-                    </Link>
-                  </div>
-                  <div className="field">
-                    <motion.button
-                      whileHover={{ scale: 1.1 }}
-                      whileTap={{ scale: 0.9 }}
-                      type="submit"
-                      className="button is-warning is-rounded"
-                    >
-                      Sign up
-                    </motion.button>
-                  </div>
-                </div>
-              </form>
-              {errorMessage && <p className="error-message">{errorMessage}</p>}
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+        </section>
+    )
 }
