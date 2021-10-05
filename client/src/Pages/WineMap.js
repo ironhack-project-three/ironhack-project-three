3190ab4a1aec38a59208b1bd0747e36c8aceaa4e
Jesse Vermeulen <jessevermeulen123@gmail.com>
2021-10-05 19:15:13 +0200

    Make things compile maybe?

--- client/src/Pages/WineMap.js
+++ client/src/Pages/WineMap.js
@@ -1,9 +1,4 @@
 import React from 'react'
-import { Wines } from "../api/wines";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { Box } from "react-bulma-components";
-
 
 export default function WineMap() {
   return (
