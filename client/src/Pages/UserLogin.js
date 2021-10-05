46cd2f4daeb23bf1476889feea1a8fcc5cd71f9b
Mathijsvanginneken <mathijsvginneken@gmail.com>
2021-10-05 20:56:49 +0200

    changes fixed

--- client/src/Pages/UserLogin.js
+++ client/src/Pages/UserLogin.js
@@ -1,131 +1,91 @@
-import React from "react";
-import { useState, useContext } from "react";
-import { AuthContext } from "../context/auth.context";
-import { motion } from "framer-motion";
-import { Users } from "../api/users";
-
+import React from 'react';
+import axios from "axios";
+import {useState, useContext } from 'react';
+import { AuthContext } from '../context/auth.context';
+import { motion } from "framer-motion"
+const API_URL = "http://localhost:3000";
 export default function UserLogin(props) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState(undefined);
-  const { logInUser } = useContext(AuthContext);
-
-  const handleEmail = (e) => setEmail(e.target.value);
-  const handlePassword = (e) => setPassword(e.target.value);
-  const handleLoginSubmit = async (e) => {
-    e.preventDefault();
-    const requestBody = { email, password };
-    const storedToken = localStorage.getItem("authToken");
-    try {
-      const response = await Users.login(requestBody, storedToken);
-      const token = response.data.authToken;
-      logInUser(token);
-      props.history.push("/");
-    } catch (error) {
-      const errorDescription = error.response.data.message;
-      setErrorMessage(errorDescription);
-    }
-  };
-
-
+        const [email, setEmail] = useState("");
+        const [password, setPassword] = useState("");
+        const [errorMessage, setErrorMessage] = useState(undefined);
+        const { logInUser } = useContext(AuthContext);
+        const handleEmail = (e) => setEmail(e.target.value);
+        const handlePassword = (e) => setPassword(e.target.value);
+        const handleLoginSubmit = (e) => { e.preventDefault();
+        const requestBody = { email, password };
+        const storedToken = localStorage.getItem('authToken');
+            axios.post(`${API_URL}/users/login`, requestBody,
+            { headers: { Authorization: `Bearer ${storedToken}`}})
+            .then((response) => {
+                const token = response.data.authToken;
+                logInUser(token);
+                props.history.push("/");
+              })
+              .catch((error) => {
+                const errorDescription = error.response.data.message;
+                setErrorMessage(errorDescription);
+              });
+          };
     return (
         <section className="hero has-background-white-ter is-fullheight">
             <div className="hero-body">
                 <div className="container">
                     <div className="columns is-centered">
                         <div className="column is-5-tablet is-4-desktop is-3-widescreen">
-                            <form 
+                            <form
                             action='/users/login'
-                            method="POST" 
-                            className="box" 
+                            method="POST"
+                            className="box"
                             onSubmit={handleLoginSubmit}>
                                 <h3 className="is-3">Login</h3>
                                 <div className="field">
-                                
                                 </div>
                                 <div className="field">
-                                    <label 
-                                    for="email" 
+                                    <label
+                                    for="email"
                                     className="label">Email</label>
                                     <div className="control has-icons-left">
-                                    <input 
-                                    type="email" 
-                                    name="email" 
-                                    placeholder="e.g. your_email@Vinevibe.com" 
-                                    className="input" 
-                                    value={email} 
-                                    onChange={handleEmail} 
+                                    <input
+                                    type="email"
+                                    name="email"
+                                    placeholder="e.g. your_email@Vinevibe.com"
+                                    className="input"
+                                    value={email}
+                                    onChange={handleEmail}
                                     required></input>
                                     </div>
                                 </div>
                                 <div className="field">
-                                    <label 
-                                    for="password" 
-                                    type="password" 
+                                    <label
+                                    for="password"
+                                    type="password"
                                     className="label">Password</label>
                                     <div className="control has-icons-left">
-                                        <input 
-                                        type="password" 
-                                        name="password" 
-                                        placeholder="********" 
-                                        className="input" 
-                                        value={password} 
-                                        onChange={handlePassword} 
+                                        <input
+                                        type="password"
+                                        name="password"
+                                        placeholder="********"
+                                        className="input"
+                                        value={password}
+                                        onChange={handlePassword}
                                         required></input>
                                     </div>
                                     <div className='field'>
                                         {/* <a href="/TODO">Forgot Password</a> */}
                                     </div>
                                     <div className="field">
-
-                                        <motion.button 
+                                        <motion.button
                                         whileHover = {{ scale: 1.1}}
                                         whileTap = {{ scale: 0.9}}
                                         className="button is-warning is-rounded" type="submit">Login</motion.button>
-
                                     </div>
                                 </div>
                             </form>
                             { errorMessage && <p className="error-message">{errorMessage}</p> }
                         </div>
                     </div>
-
                 </div>
-                <div className="field">
-                  <label for="password" type="password" className="label">
-                    Password
-                  </label>
-                  <div className="control has-icons-left">
-                    <input
-                      type="password"
-                      name="password"
-                      placeholder="********"
-                      className="input"
-                      value={password}
-                      onChange={handlePassword}
-                      required
-                    ></input>
-                  </div>
-                  <div className="field">
-                    {/* <a href="/TODO">Forgot Password</a> */}
-                  </div>
-                  <div className="field">
-                    <motion.button
-                      whileHover={{ scale: 1.1 }}
-                      whileTap={{ scale: 0.9 }}
-                      className="button is-warning is-rounded"
-                      type="submit"
-                    >
-                      Login
-                    </motion.button>
-                  </div>
-                </div>
-              </form>
-              {errorMessage && <p className="error-message">{errorMessage}</p>}
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+        </section>
+    )
 }
