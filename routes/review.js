a4787a74e1d85689bbb6c3ef488f7909922c2730
Coops023 <bjorkc27@gmail.com>
2021-10-05 15:38:25 +0200

    update

--- routes/review.js
+++ routes/review.js
@@ -6,16 +6,37 @@ const router = require('express').Router();
 const { isAuthenticated } = require("../middleware/jwt.middleware");
 
 // GET to show review form
-router.get('/create/:wineId', (req, res) => {
-    const { wineId } = req.params;
-    res.status(200).json('reviews/new-review', { wineId });
-  });
+// router.get('/create/:wineId', (req, res) => {
+//     const { wineId } = req.params;
+//     res.status(200).json({ wineId })
+//     .then(()=>{
+//       Review.create({ comment })
+//         .then((createdReview) => {
+//           return Wine.findByIdAndUpdate(
+//             wineId,
+//             {
+//               $push: { reviews: createdReview._id },
+//             },
+//             {
+//               new: true,
+//             }
+//           );
+//         })
+//         .then(()=>{
+//             console.log(res.status(200)) 
+//         })
+//         .catch((err) => {
+//           console.log(err);})
+
+//     })
+//   });
   
   // POST to pubslish a review
   router.post('/create',  async (req, res) => {
-    const { comment, wineId } = req.body;
+    const {user, comment, wineId } = req.body;
     const foundWine = await Wine.findById(wineId);
-      Review.create({ comment })
+    console.log(foundWine)
+      Review.create({ user ,comment })
         .then((createdReview) => {
           return Wine.findByIdAndUpdate(
             wineId,
@@ -28,11 +49,17 @@ router.get('/create/:wineId', (req, res) => {
           );
         })
         .then(()=>{
-           console.log(res.status(200)) 
+            console.log("review saved to database!") 
         })
         .catch((err) => {
           console.log(err);})
 
   });
+
+
+  router.put("/add-wine-review/:wineId", (req, res) => {
+      const {wineId} = req.params
+      console.log(wineId)
+  })
   
   module.exports = router;
