9185c7375635a5c612f7411c0a3b8489e25e06df
Coops023 <bjorkc27@gmail.com>
2021-10-05 10:18:59 +0200

    add new wine form submit fixed

--- client/src/api/users.js
+++ client/src/api/users.js
@@ -1,8 +1,8 @@
 const axios = require("axios");
-export class Wines {
+export class Users {
   constructor(baseURL) { //baseUrl could be overwritten in the route that uses the API
     const baseUrl = process.env.REACT_APP_API_URL || "http://localhost:3000"
-    this.baseURL = `${baseUrl}/wines` 
+    this.baseURL = `${baseUrl}/users` 
     this.api = axios.create(
       {
         baseURL: this.baseURL
@@ -11,13 +11,11 @@ export class Wines {
   }
 
   // // CHANGE THE PATHS ACCORDING TO API DOCUMENTATION
-  getAll = () => this.api.get("/all-wine")
-  getOne = (id)=> this.api.get(`/wine/${id}`)
-  search = (query)=> this.api.get(`/search/?q=${query}`)
+  getAll = () => this.api.get("/all-users")
+  getOne = (id)=> this.api.get(`/user/${id}`)
+//   search = (query)=> this.api.get(`/search/?q=${query}`)
   // createOne = (newEntityValues)=>this.api.post("/", newEntityValues)
   // deleteOne = (id)=> this.api.delete(`/${id}`)
   // updateOne = (id)=> this.api.put(`/${id}`)
   // // etc...
 }
-
-// export default Wines;
