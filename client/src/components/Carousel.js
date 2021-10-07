db94029d380a62dc67c6d72d8efa386c951fcbaa
Jesse Vermeulen <jessevermeulen123@gmail.com>
2021-10-07 17:31:18 +0200

    Code fixes

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -4,12 +4,14 @@ import  {Api}  from "../api/api";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import './carousel.css';
+import ladyWithRose from "../Images/ladyWithRose.jpg"
+import glassesOfRedAndRose from "../Images/glassesOfRedAndRose.jpg"
+import rosePour from "../Images/rosePour.jpg"
+import wineLemons from "../Images/wineLemons.jpg"
+import winePeaches from "../Images/winePeaches.jpg"
 
 export default function ImageCarousel(props) {
 
-
-  
-  
   const [image, setImage] = useState([]);
   async function fetchImage() {
     const response = await new Api().getAll();
@@ -26,20 +28,22 @@ export default function ImageCarousel(props) {
   console.log("line 22", image)
 
   return (
-  
-      
-<Carousel className="Carousel" dynamicHeight={true} autoPlay infiniteLoop width={"100%"} showThumbs={false}>
+   
+<Carousel className="Carousel" autoPlay infiniteLoop width={"100%"} showThumbs={false}>
   <div>
-      <img alt="wine" src={image[0]} />
+      <img alt="wine" src={ladyWithRose} />
   </div>
   <div>
-      <img alt="wine" src={image[1]} />
+      <img alt="wine2" src={winePeaches} />
   </div>
   <div>
-      <img alt="wine" src={image[3]} />
+      <img alt="wine3" src={glassesOfRedAndRose} />
+  </div> 
+  <div>
+      <img alt="wine4" src={rosePour} />
   </div>
   <div>
-      <img alt="wine" src={image[7]} />
+      <img alt="wine5" src={wineLemons} />
   </div>
 </Carousel>
   
