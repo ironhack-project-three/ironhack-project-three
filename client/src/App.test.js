31849c1b9212c78a171b78e3101bd46426fde04c
zoidbergwill <zoidbergwill@users.noreply.github.com>
2021-10-20 00:04:18 +0200

    Try make the test happier

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Wine of the month/i);
   expect(linkElement).toBeInTheDocument();
 });
