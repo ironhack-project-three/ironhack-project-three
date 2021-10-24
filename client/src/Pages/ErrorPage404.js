017ce10d1c1b29e6b38b1a2240c8da5eb780e3c4
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2021-10-24 15:10:38 +0200

    Create 404 message

--- client/src/Pages/ErrorPage404.js
+++ client/src/Pages/ErrorPage404.js
@@ -6,6 +6,7 @@ export default function ErrorPage404() {
     return (
         <div className="error-page is-flex m-auto">  
             <h1 className="errorText">Error 404</h1>
+            <p>Sorry, the page you are looking for does not exist.<p>
             <img src={errorImage} alt="404" className="errorImage2" />
         </div>
     )
